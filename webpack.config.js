3ce87d6d16264416d471f24418c426b79f265657
luijocroussett <ljcroussett@gmail.com>
2019-03-29 20:09:42 -0400

    completed webpack and initial setup

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    login: './client/index.js'
+    },
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/build'),
+    port: 3000,
+    hot: true,
+    proxy: {
+      '/': 'http://localhost:3000'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /css/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
+};
