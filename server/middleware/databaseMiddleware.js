b443678fd585c0dc5333dfc2c5ce458182d1d074
luijocroussett <ljcroussett@gmail.com>
2019-03-30 16:15:49 -0400

    fixed errors

--- server/middleware/databaseMiddleware.js
+++ server/middleware/databaseMiddleware.js
@@ -3,19 +3,25 @@ const { Client } = require('pg');
 
 const client = new Client();
 
-databaseMiddleware.saveUserInfo = (req, res, next) => {
+databaseMiddleware.saveUserInfo = async (req, res, next) => {
   await client.connect();
-  const {email, token} = req.body;
-  const result = await client.query(`INSERT INTO (email, accessToken) VALUES (${email}, ${token});`);
+  const { email, token } = req.body;
+  const result = await client.query(
+    `INSERT INTO (email, accessToken) VALUES (${email}, ${token});`,
+  );
   await client.end();
   next();
 };
 
-databaseMiddleware.getUserProjects = (req, res, next) => {
+databaseMiddleware.getUserProjects = async (req, res, next) => {
   await client.connect();
   let cookie = req.cookie;
-  res.body.email = await client.query(`SELECT email FROM users WHERE sessionid=${res.cookie}`)
-  res.body.result = await client.querry(`SELECT projectname, id FROM users WHERE email=${res.body.email}`);
+  res.body.email = await client.query(
+    `SELECT email FROM users WHERE sessionid=${res.cookie}`,
+  );
+  res.body.result = await client.querry(
+    `SELECT projectname, id FROM users WHERE email=${res.body.email}`,
+  );
   await client.end();
   next();
 };
